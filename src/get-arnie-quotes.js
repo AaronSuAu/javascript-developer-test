d537b46441b92a0991937c0bc2ae1cbaac7b4f3e
Aaron <aaron@Aarons-MacBook-Pro.local>
2026-05-26 11:32:14 +1000

    Implement getArnieQuotes

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -5,7 +5,13 @@ const { httpGet } = require('./mock-http-interface');
  * @returns {ArnieQuoteResponse}
  */
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
+  return Promise.all(
+    urls.map(async (url) => {
+      const { status, body } = await httpGet(url);
+      const { message } = JSON.parse(body);
+      return status === 200 ? { 'Arnie Quote': message } : { 'FAILURE': message };
+    })
+  );
 };
 
 module.exports = {
